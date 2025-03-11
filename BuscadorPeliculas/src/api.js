6bc5f54d5b81c197efaaf04293c8e3ea0cccf49c
morales80 <164915603+morales80@users.noreply.github.com>
2025-03-11 17:52:11 -0600

    [UPDATE]:se actualizo el api.js

--- BuscadorPeliculas/src/api.js
+++ BuscadorPeliculas/src/api.js
@@ -1,13 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import './assets/estilos/App.css';
+import Contenedor from './components/contenedor';
+import { Card } from './components/card';
 import axios from 'axios';
 
-const API_URL = 'https://api.themoviedb.org/3/movie/popular';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY; // Correcto para Vite
 
-const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`
-  }
-});
+function App() {
+  const [movies, setMovies] = useState([]);
 
-export default api;
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/movie/popular', {
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+      }
+    })
+    .then(response => {
+      setMovies(response.data.results);
+    })
+    .catch(error => {
+      console.error('Error al obtener los datos:', error);
+    });
+  }, []);
+
+  return (
+    <Contenedor>
+      {movies.map(movie => (
+        <Card
+          key={movie.id}
+          imagen={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          name={movie.title}
+          precio_ant="300"  
+          precio="250"  
+        />
+      ))}
+    </Contenedor>
+  );
+}
+
+export default App;

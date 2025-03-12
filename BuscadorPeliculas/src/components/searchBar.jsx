3572f896732fc24a7e5a2fd5ef5f945863684c4a
daeeraleylani <dairaleylani@gmail.com>
2025-03-12 17:42:47 -0600

    Fin de clase

--- BuscadorPeliculas/src/components/searchBar.jsx
+++ BuscadorPeliculas/src/components/searchBar.jsx
@@ -2,7 +2,7 @@ import './../assets/estilos/searchBar.css'
 function SearchBar(){
     return(
         
-            <input type="text"  className='Buscador' placeholder='Ingresa nombre, género o actor para buscar...'/>
+            <input type="text"  className='Buscador' placeholder='Ingresa nombre, género o actor para buscar....'/>
     )
 }
 

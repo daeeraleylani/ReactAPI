1dfc3c37ee13b032a117e026f94025fff5f02837
daeeraleylani <dairaleylani@gmail.com>
2025-03-18 17:11:27 -0600

    [UPDATE]:Modificacion al estilo del contenedor con posición corregida de la barra de busqueda

--- BuscadorPeliculas/src/components/contenedor.jsx
+++ BuscadorPeliculas/src/components/contenedor.jsx
@@ -2,8 +2,7 @@ import './../assets/estilos/contenedor.css'
 
 function Contenedor(prop){
     return(
-        <section className="seccion_Contactos">
-            
+        <section className="seccion_Contactos">             
             <h1 className="seccion_Titulo">Encuentra tus películas favoritas aquí</h1>
             <div className="section_contactos_datos"> 
                 {prop.children}

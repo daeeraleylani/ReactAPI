66df590dfcb3b10717e5a23c184e2f4533287745
Luis Carlos <ruberrodriguezcarlos@gmail.com>
2025-03-19 16:28:35 -0600

    [ADD]:se añadieron cambios a searchBar.jsx

--- BuscadorPeliculas/src/components/searchBar.jsx
+++ BuscadorPeliculas/src/components/searchBar.jsx
@@ -1,11 +1,17 @@
-import './../assets/estilos/searchBar.css'
-function SearchBar() {
+import './../assets/estilos/searchBar.css';
+
+function SearchBar({ searchTerm, handleSearchChange }) {
     return (
         <section className='box'>
-            <input type="text" className='Buscador-Input' placeholder='🔎'/>
+            <input 
+                type="text" 
+                className='Buscador-Input' 
+                placeholder='🔎 Buscar por nombre o género' 
+                value={searchTerm} 
+                onChange={handleSearchChange} 
+            />
         </section>
-
-    )
+    );
 }
 
-export default SearchBar    
+export default SearchBar;

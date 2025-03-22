b9074ef5b5123ce0003c0ca415563e8481bad4c4
Luis Carlos <ruberrodriguezcarlos@gmail.com>
2025-03-22 15:34:16 -0600

    [ADD]: se agrego una imagen por defecto en créditos al archivo card.jsx

--- BuscadorPeliculas/src/components/card.jsx
+++ BuscadorPeliculas/src/components/card.jsx
@@ -38,7 +38,7 @@ function Card({ id, name, imagen, overview, releaseDate, voteAverage, genreIds, 
                 character: person.character,
                 image: person.profile_path 
                     ? `https://image.tmdb.org/t/p/w200${person.profile_path}` 
-                    : "https://via.placeholder.com/100"
+                    : "https://png.pngtree.com/png-vector/20221125/ourmid/pngtree-no-image-available-icon-flatvector-illustration-thumbnail-graphic-illustration-vector-png-image_40966590.jpg"
             }));
 
             let castHtml = `
